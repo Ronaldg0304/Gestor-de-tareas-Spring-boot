e33d3f5b71832d0d4edfbe64a42999f5e153aa3f
Ronald Guerra <ronaldg0304@gmail.com>
2025-01-09 08:45:50 -0500

    Corrección interface TaskRepository

--- src/main/java/com/springboot/gestortareas/gestor_tareas/repositories/TaskRepository.java
+++ src/main/java/com/springboot/gestortareas/gestor_tareas/repositories/TaskRepository.java
@@ -1,8 +1,9 @@
 package com.springboot.gestortareas.gestor_tareas.repositories;
 
+import com.springboot.gestortareas.gestor_tareas.entities.Task;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TaskRepository extends CrudRepository {
+public interface TaskRepository extends CrudRepository <Task, Long> {
 
 
 }
