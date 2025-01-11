ba111bc97df0e304ad1dda668d59779289c5fa19
Ronald Guerra <ronaldg0304@gmail.com>
2025-01-11 10:31:45 -0500

    Se inserta anotación @Data para clase Task y se crea un atributo updateAt con la anotación @PreUpdate

--- src/main/java/com/springboot/gestortareas/gestor_tareas/entities/Task.java
+++ src/main/java/com/springboot/gestortareas/gestor_tareas/entities/Task.java
@@ -1,34 +1,93 @@
 package com.springboot.gestortareas.gestor_tareas.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@NoArgsConstructor
+/*@NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Setter
-@ToString
+@Setter*/
+@Data
 @Entity
-@Table(name= "tasks")
+@Table(name = "tasks")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "title", nullable = false, length = 50)
     private String title;
 
-    @Column(nullable = false)
+    @Column(name = "description", nullable = false, length = 150)
     private String description;
 
-    @Column(nullable = false)
-    private boolean stateTask;
+    @Column(name = "completed")
+    private boolean completed;
 
-    @Column(nullable = false)
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now(); // Asigna la fecha de creación antes de insertar en la base de datos
+        updatedAt = LocalDateTime.now();
+    }
+
+   /* public Task() {
+    }
+
+    public Task(String title, String description, boolean completed) {
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }*/
 }
 
 
