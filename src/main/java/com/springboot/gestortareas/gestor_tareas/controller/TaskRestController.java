f223abbda04d8e3358c8c098ed20d06579c7f97a
Ronald Guerra <ronaldg0304@gmail.com>
2025-01-14 08:39:10 -0500

    Refactor: se implementa metodo findtaskById para evitar codigo repetitivo en los endpoints  con control de excepcion TaskNotFoundException

--- src/main/java/com/springboot/gestortareas/gestor_tareas/controller/TaskRestController.java
+++ src/main/java/com/springboot/gestortareas/gestor_tareas/controller/TaskRestController.java
@@ -1,7 +1,8 @@
 package com.springboot.gestortareas.gestor_tareas.controller;
 
-import com.springboot.gestortareas.gestor_tareas.DTO.ApiResponse;
+import com.springboot.gestortareas.gestor_tareas.dto.ApiResponse;
 import com.springboot.gestortareas.gestor_tareas.entities.Task;
+import com.springboot.gestortareas.gestor_tareas.errors.TaskNotFoundException;
 import com.springboot.gestortareas.gestor_tareas.services.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -9,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/tasks")
@@ -21,49 +21,44 @@ public class TaskRestController {
         this.service = service;
     }
 
-    @GetMapping //
+    @GetMapping
     public ResponseEntity<ApiResponse<List<Task>>> getAllTasks() {
         List<Task> tasks = service.findAll();
-        return ResponseEntity.ok(new ApiResponse<>("Tasks retrieved successfully", tasks));
+        return ResponseEntity.ok(new ApiResponse<>("Tasks retrieved successfully", tasks, HttpStatus.OK));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<Task>> detail(@PathVariable Long id) {
-        Optional<Task> optionalTask = service.findById(id);
-        return optionalTask.map(task -> ResponseEntity.ok(new ApiResponse<>("Task found", task))).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>("Task not found", null)));
+    public ResponseEntity<ApiResponse<Task>> detail(@PathVariable Long id) throws TaskNotFoundException {
+        Task task = findTaskById(id);
+        return ResponseEntity.ok(new ApiResponse<>("Task retrieved successfully", task, HttpStatus.OK));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<Task>> update(@Valid @RequestBody Task task, @PathVariable Long id) {
-        Optional<Task> optionalTask = service.findById(id);
-        if (optionalTask.isPresent()) {
-            Task taskDb = optionalTask.get();
-            taskDb.setTitle(task.getTitle());
-            taskDb.setDescription(task.getDescription());
-            taskDb.setCompleted(task.isCompleted());
-            Task updatedTask = service.save(taskDb);
-            return ResponseEntity.ok(new ApiResponse<>("Task updated successfully", updatedTask));
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ApiResponse<>("Task not found", null));
+    public ResponseEntity<ApiResponse<Task>> update(@Valid @RequestBody Task task, @PathVariable Long id) throws TaskNotFoundException {
+        Task taskDb = findTaskById(id);
+        taskDb.setTitle(task.getTitle());
+        taskDb.setDescription(task.getDescription());
+        taskDb.setCompleted(task.isCompleted());
+        Task updatedTask = service.save(taskDb);
+        return ResponseEntity.ok(new ApiResponse<>("Task details updated successfully", updatedTask, HttpStatus.CREATED));
     }
 
     @PostMapping
     public ResponseEntity<ApiResponse<Task>> create(@Valid @RequestBody Task task) {
         Task savedTask = service.save(task);
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new ApiResponse<>("Task created successfully", savedTask));
+                .body(new ApiResponse<>("Task created successfully", savedTask, HttpStatus.CREATED));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<Void>> delete(@PathVariable Long id) {
-        Optional<Task> optionalTask = service.findById(id);
-        if (optionalTask.isPresent()) {
-            service.deleteById(id);
-            return ResponseEntity.ok(new ApiResponse<>("Task deleted successfully", null));
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ApiResponse<>("Task not found", null));
+    public ResponseEntity<ApiResponse<Task>> delete(@PathVariable Long id) throws TaskNotFoundException {
+        Task taskDb = findTaskById(id);
+        service.deleteById(taskDb.getId());
+        return ResponseEntity.ok(new ApiResponse<>("Task deleted successfully", taskDb, HttpStatus.OK));
     }
 
+    private Task findTaskById(Long id) throws TaskNotFoundException {
+        Task taskFound = service.findById(id).get();
+        return taskFound;
+    }
 }
